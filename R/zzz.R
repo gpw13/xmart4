6cdeeeb0a724150d012d7a0ebda061985b9e2675
ElliottMess <ElliottMess@users.noreply.github.com>
2022-01-23 12:31:23 +0000

    Built site for xmart4: 0.2.2@77d8963

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-# environment to store API tokens
-.xmart_env <- new.env(parent = emptyenv())
-
-# user agent
-ua <- httr::user_agent("https://github.com/caldwellst/xmart4")
-
-.onLoad <- function(lib, pkg) {
-  xmart4_api <<- memoise::memoise(xmart4_api)
-}
